dd1f478d9282a077ad0fbcdf03f08fe71f4989af
PERFECTION <akinbinuayomide1@gmail.com>
2025-06-05 01:26:37 +0100

    adjusted cart css and logout

--- js/nav.js
+++ js/nav.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .addEventListener("click", function (e) {
         e.preventDefault();
         localStorage.clear();
-        window.location.href = "login.html";
+        window.location.href = "index.html";
       });
   }
 });
